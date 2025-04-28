713f23ceac9d936ae09e59ffe006ce27b6190b62
BrunoEtec033 <brunosououro3@gmail.com>
2025-04-27 23:10:02 -0300

    ajientaod os drawer

--- App.js
+++ App.js
@@ -1,46 +1,50 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import Galeria from './component/Galeria';
 import Perfil1 from './component/Perfil1';
 import Perfil2 from './component/Perfil2';
 import Perfil3 from './component/Perfil3';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-const App = () => {
+const App = ()=>{
   return (
     <NavigationContainer>
-      <Tab.Navigator
+
+      <Drawer.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, size }) => {
+          drawerIcon: ({ focused, size }) => {
             let iconName;
-            if (route.name === 'Minnesota Timberwolves') {
+            if (route.name === 'Perfil1') {
               iconName = focused ? 'person' : 'person-outline';
-            } else if (route.name === 'Oklahoma City Thunder') {
+            } else if (route.name === 'Perfil2') {
               iconName = focused ? 'person' : 'person-outline';
-            } else if (route.name === 'Boston Celtics') {
+            } else if (route.name === 'Perfil3') {
               iconName = focused ? 'person' : 'person-outline';
             }
             return <Ionicons name={iconName} size={size} color={focused ? '#007AFF' : '#8e8e93'} />;
           },
         })}
       >
-        <Tab.Screen name="Minnesota Timberwolves" component={Perfil1} />
-        <Tab.Screen name="Oklahoma City Thunder" component={Perfil2} />
-        <Tab.Screen name="Boston Celtics" component={Perfil3} />
-      </Tab.Navigator>
+        <Drawer.Screen name="Minnesota Timberwolves" component={Perfil1} />
+        <Drawer.Screen name="Oklahoma City Thunder" component={Perfil2} />
+        <Drawer.Screen name="Boston Celtics" component={Perfil3} />
+      </Drawer.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
-};
+}
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+}); 
